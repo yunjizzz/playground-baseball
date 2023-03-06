b5b6e6d837681cbeb2d6fca55386aed0d7bf47df
yunjizzz <60842443+yunjizzz@users.noreply.github.com>
2023-03-06 23:45:18 +0900

    feat : add test code ing

---
+++ src/test/java/study/BaseballGameTest.java
@@ -0,0 +1,41 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseballGameTest {
+
+
+
+    @Test
+    void playBaseBall() {
+    }
+
+    @Test
+    @DisplayName("3자리 난수를 생성한다. 중복되면 안된다.")
+    void makeRandomNumber() {
+
+    }
+
+    @Test
+    void strikeCheck() {
+    }
+
+    @Test
+    void strikeCharEquals() {
+    }
+
+    @Test
+    void ballCheck() {
+    }
+
+    @Test
+    void ballCharContains() {
+    }
+
+    @Test
+    void makeString() {
+    }
+}
